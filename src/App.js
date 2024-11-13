7d875f0cab84935eec0ce48ce21867a2772d61ce
Kevin Dougan <72025369+KevinDougan@users.noreply.github.com>
2024-11-13 10:24:55 -0500

    Update src/App.js
    Co-authored-by: Mohammad Farooqi <mohammad.farooqi@gmail.com>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
     });
   }
   render() {
-/*    return <p>Check the console!</p>; */
     return (
       <div className="App">
         <header className="App-header">
